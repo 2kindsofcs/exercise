486732f77c86c1291427fc6990367eca4a2cbde7
2kindsofcs <2kindsofcs@gmail.com>
2019-01-08 19:40:55 +0900

    add footnote

--- python exercise/insertion sort.py
+++ python exercise/insertion sort.py
@@ -2,10 +2,10 @@ import unittest
 
 def insertionSort(list): # 카멜표기법으로 함수 이름 작성
     n = len(list)
-    for i in range(1,n):
+    for i in range(1,n):  # 1번째 요소(list[0])를 기준으로 2번째 요소(list[1])부터 비교 시작
         value = list[i]
         position = i
-        while position > 1 and list[position-1] > value:
+        while position > 1 and list[position-1] > value: # value보다 작은 값을 가진 위치에 도달하면 while문을 빠져나간다
             list[position] = list[position-1]
             position -= 1
         list[position] = value      
@@ -27,4 +27,3 @@ class testInsertionSort(unittest.TestCase):
 
 if __name__ == '__main__':
         unittest.main()
-
