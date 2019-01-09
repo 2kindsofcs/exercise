e74c27b0f235989b53f307762bb9075bef377e43
2kindsofcs <2kindsofcs@gmail.com>
2019-01-09 23:58:12 +0900

    conditional statements exercise

---
+++ python exercise/Intro to conditional statements.py
@@ -0,0 +1,21 @@
+# 30 days of code
+# https://www.hackerrank.com/challenges/30-conditional-statements/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    N = int(input())
+
+if N % 2 == 0:
+    if N in range(6,21):
+        print("Weird")
+    else:
+        print("Not Weird")
+else:
+    print("Weird")    
