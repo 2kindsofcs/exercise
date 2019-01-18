d35a6c4d0f0c85a81e00531c8376603e99fe70c1
2kindsofcs <42531877+2kindsofcs@users.noreply.github.com>
2019-01-19 01:05:58 +0900

    improve quality of code
    use variables

--- python exercise/Time Conversion.py
+++ python exercise/Time Conversion.py
@@ -3,23 +3,30 @@
 import os
 import sys
 
-#
-# Complete the timeConversion function below.
-#
+
 def timeConversion(s):
-    if s[-2:] == "PM":
-        if s[0:2] == "12":
-            return s[0:8]
-        else:
-            hour = int(s[0:2]) + 12
-            result = str(hour) + s[2:8]
-            return result
-    else:
-        return s[0:8]
+	hour = s[:2]
+	am_pm = s[-2:]
+	min_sec = s[2:-2]
 
+	if am_pm == "AM" and hour == "12":
+		hour = "00"
+	elif am_pm == "PM" and hour != "12":
+		hour = str(int(hour) + 12)
+	return hour + min_sec
 
-    # if iis am then leave it alone
-    # if its pm then plus 12(h)
+# AM and 12? -> hour=00 +minsec
+# PM and not 12 -> hour+12  + minsec
+# others: return hour + minsec
+# 뭔가 반복되는 게 쌔하다 -> 변수에 담을 수 있는지 확인할 것
+# 뭔가 반복되는 게 쌔하다(2) -> 사실상 한 가지 경우인데 너무 잘게 쪼개지 않았는지 확인할 것 (결국 앞의 것과 같은 맥락이지만)
+
+# print(timeConversion("12:00:00AM"))
+# print(timeConversion("01:20:00PM"))
+# print(timeConversion("01:00:00AM"))
+# print(timeConversion("04:23:00PM"))
+# print(timeConversion("12:30:00PM"))
+
 
 if __name__ == '__main__':
     f = open(os.environ['OUTPUT_PATH'], 'w')
