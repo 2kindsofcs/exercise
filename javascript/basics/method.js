aef4c7655c6d767d873b8b57eca53355c545190d
2kindsofcs <2kindsofcs@gmail.com>
2019-01-21 16:12:25 +0900

    improve quality of code(2)

--- javascript/basics/method.js
+++ javascript/basics/method.js
@@ -23,12 +23,9 @@ console.log(restaurant.checkAvailability(4))
 // 내장 메소드 사용하기 
 
 let isValidPassword = function (password) { 
-    if (password.length > 8 && !password.includes('password')){ // ===false 대신 앞에 ! 붙여서 보다 간결하게
-        return true
-    }
-    else {
-        return false
-    }
+    return (password.length > 8 && !password.includes('password')){ // ===false 대신 앞에 ! 붙여서 보다 간결하게
+    // 위 두 조건이 동시에 충족되지 않았을 경우, false를 리턴하는 것 외의 행위가 전혀 없으므로
+    // 이 조건 판단의 결과 자체를 그대로 return해도 상관이 없다. 
 }
 
 console.log(isValidPassword("23423423password344"))
