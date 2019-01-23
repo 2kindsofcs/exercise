80144aa89121ad44936934b93f23209ecab85db9
2kindsofcs <2kindsofcs@gmail.com>
2019-01-23 23:37:39 +0900

    function exercise

--- javascript/basics/arrays/notes.js
+++ javascript/basics/arrays/notes.js
@@ -1,4 +1,23 @@
-const notes = ['Note 1', 'Note 2', 'Note 3']
+const notes = [{
+    title: 'My next trip',
+    body: 'I;d like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better.'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
+
+const findNote = function (notes, noteTitle) {
+    const index = notes.findIndex(function (note, index) {
+        return note.title.toLowerCase() === noteTitle.toLowerCase()
+    })
+    return notes[index]
+}
+
+const note = findNote(notes, 'My next trip')
+console.log(note)
 
 // console.log(notes.pop())
 // notes.push("my new note!")
@@ -13,6 +32,12 @@ const notes = ['Note 1', 'Note 2', 'Note 3']
 //     console.log(item)
 // })
 
-for (let count = 0; count <= 2; count = count +1) {
-    console.log(count)
-}
+// for (let count = 0; count <= 2; count = count +1) {
+//     console.log(count)
+// }
+
+// for (let count = notes.length - 1; count >= 0; count--) {
+//     console.log(notes[count])
+// }
+
+// console.log(notes.indexOf('Note 1'))
--- javascript/basics/arrays/todo.js
+++ javascript/basics/arrays/todo.js
@@ -1,14 +1,18 @@
 let todo = ['lunch', 'reading', 'exercise', 'yoga', 'brewing'];
 
-todo.splice(2,1);
-todo.push("drink coffee");
-todo.shift();
+// todo.splice(2,1);
+// todo.push("drink coffee");
+// todo.shift();
 
-console.log(todo);
-console.log(`you have ${todo.length-1} todos.`);
-console.log(`todo 1: ${todo[1]}`);
+// console.log(todo);
+// console.log(`you have ${todo.length-1} todos.`);
+// console.log(`todo 1: ${todo[1]}`);
 
-todo.forEach(function (item, index){
-    const num = index + 1
-    console.log(`${num}.${item}`)
-})
+// todo.forEach(function (item, index){
+//     const num = index + 1
+//     console.log(`${num}.${item}`)
+// })
+
+for (let count = 0; count < todo.length; count++){
+    console.log(`${count + 1}. ${todo[count]}`)
+}
