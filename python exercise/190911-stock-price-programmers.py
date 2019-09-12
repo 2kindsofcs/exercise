1d257e6dcced5cd0458403f846f83140b457cf7e
2kindsofcs <42531877+2kindsofcs@users.noreply.github.com>
2019-09-12 21:49:50 +0900

    Update 190911-stock-price-programmers.py

--- python exercise/190911-stock-price-programmers.py
+++ python exercise/190911-stock-price-programmers.py
@@ -1,20 +1,14 @@
 def solution(prices):
     length = len(prices)
-    minima, answer = [], []
-    for i in range(1, length):
-        if prices[i] - prices[i - 1] < 0:
-            minima.append(i)
-    for i in range(length):
-        for index in minima:
-            if index > i and prices[i] > prices[index]:
-                answer.append(index - i)
+    answer = [ length - i - 1 for i in range(length) ]
+    for i in range(length - 1):
+        for j in range(i + 1, length):
+            if prices[i] > prices[j]:
+                answer[i] = j - i
                 break
-        else:
-            answer.append(length - i - 1)
     return answer
-    
-    
+
 # 채점 결과
 # 정확성: 66.7
-# 효율성: 0.0
-# 합계: 66.7 / 100.0
+# 효율성: 33.3
+# 합계: 100.0 / 100.0
