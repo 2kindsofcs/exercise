953ae6f586403a5a428dc7859db610f3b4ef3b6d
2kindsofcs <42531877+2kindsofcs@users.noreply.github.com>
2019-09-23 19:32:18 +0900

    Update 190922-permutation-in-string.py

--- python exercise/190922-permutation-in-string.py
+++ python exercise/190922-permutation-in-string.py
@@ -1,18 +1,20 @@
-import itertools
+import string
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1length = len(s1)
-        if s1length > len(s2):
-            return False
-        if s1 in s2:
-            return True
-        s1List = []
+        s1Length = len(s1)
+        s2Length = len(s2)
+        alphabet = string.ascii_lowercase
+        s1Letter = [ 0 for _ in range(26) ]
         for letter in s1:
-            s1List.append(letter)
-        permuList = list(map(''.join, itertools.permutations(s1List)))
-        for permu in permuList:
-            if permu in s2:
+            index = alphabet.index(letter)
+            s1Letter[index] += 1
+        for i in range(0, s2Length - s1Length + 1):
+            s2Letter = [ 0 for _ in range(26) ]
+            for j in range(i, i + s1Length):
+                index = alphabet.index(s2[j])
+                s2Letter[index] += 1
+            if s1Letter == s2Letter:
                 return True
         return False
         
-# time limit exceeded
+# 여전히 time limit exceeded
