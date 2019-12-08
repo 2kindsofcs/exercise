85e4639a1daac4d6eb9f117dfe6d5ed25666a547
2kindsofcs <42531877+2kindsofcs@users.noreply.github.com>
2019-12-08 21:22:39 +0900

    Update 191207-pairs-of-songs-with-total-durations-divisible-by-60.py

--- python exercise/191207-pairs-of-songs-with-total-durations-divisible-by-60.py
+++ python exercise/191207-pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -1,13 +1,15 @@
 class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
         length = len(time)
+        answer = 0
+        countList = [0] * 60
         for i in range(length):
-            time[i] = time[i] % 60
-        count = 0
-        for i in range(length-1):
-            for j in range(i+1, length):
-                if (time[i] + time[j]) % 60 == 0:
-                    count += 1
-        return count 
-        
-# time limit exceeded
+            index = time[i] % 60
+            answer += countList[(60 - index) % 60]
+            countList[index] += 1
+        return answer
+    
+# Runtime: 240 ms, faster than 94.91% of Python3 online submissions for Pairs of Songs With Total Durations Divisible by 60.
+# Memory Usage: 16.3 MB, less than 9.09% of Python3 online submissions for Pairs of Songs With Total Durations Divisible by 60.
+# Next challenges:
+# 문제를 엉뚱하게 풀고 있었다. 오늘 다시 보니 문제가 제대로 이해되었다.
