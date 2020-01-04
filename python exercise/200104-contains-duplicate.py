0165a07f513ac096dabf541dc978e0b587672cf1
2kindsofcs <42531877+2kindsofcs@users.noreply.github.com>
2020-01-04 22:37:35 +0900

    Update 200104-contains-duplicate.py

--- python exercise/200104-contains-duplicate.py
+++ python exercise/200104-contains-duplicate.py
@@ -1,11 +1,11 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        nums.sort()
-        length = len(nums)
-        for i in range(1, length):
-            if nums[i] == nums[i-1]:
-                return True
+        listLength = len(nums)
+        setLength = len(set(nums))
+        if setLength < listLength:
+            return True
         return False
-
-# Runtime: 132 ms, faster than 42.29% of Python3 online submissions for Contains Duplicate.
-# Memory Usage: 17.8 MB, less than 88.68% of Python3 online submissions for Contains Duplicate.
+    
+# Runtime: 120 ms, faster than 93.86% of Python3 online submissions for Contains Duplicate.
+# Memory Usage: 18 MB, less than 88.68% of Python3 online submissions for Contains Duplicate.
+# set을 쓰는 게 더 비용이 비쌀 줄 알았는데 더 빠르다. 
