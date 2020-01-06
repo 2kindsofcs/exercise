ac8b83a8985766bae6647ccb9d64224a858e8c30
2kindsofcs <42531877+2kindsofcs@users.noreply.github.com>
2020-01-06 22:39:34 +0900

    Update 200105-duplicate-zeros.py

--- python exercise/200105-duplicate-zeros.py
+++ python exercise/200105-duplicate-zeros.py
@@ -13,8 +13,17 @@ class Solution:
             add = 1
             if arr[i] == 0:
                 add = 2
-            count += add 
+            count += add
+        modified = []
         for i in range(endIndex):
+            modified.append(arr[i])
             if arr[i] == 0:
-                # in-place로 해야하는데...
-        
+                modified.append(0)
+        if modified:
+            for i in range(length):
+                arr[i] = modified[i]
+                
+# Runtime: 64 ms, faster than 93.12% of Python3 online submissions for Duplicate Zeros.
+# Memory Usage: 13.5 MB, less than 100.00% of Python3 online submissions for Duplicate Zeros.
+# 이게 정말 문제 의도대로 푼 게 맞는건가?
+# 출제자는 다른 메모리를 할당해서 쓰는 걸 원하지 않았을 것 같은데...
