ec7837a2d6af02e4d01b483af1d1300c9f86ba28
2kindsofcs <42531877+2kindsofcs@users.noreply.github.com>
2020-01-11 23:36:34 +0900

    Create 200111-sum-of-two-integers.py

---
+++ python exercise/200111-sum-of-two-integers.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        numList = [a, b]
+        return sum(numList)
+        
+        
+# Runtime: 20 ms, faster than 96.96% of Python3 online submissions for Sum of Two Integers.
+# Memory Usage: 12.7 MB, less than 100.00% of Python3 online submissions for Sum of Two Integers.
+# ?? 이건 출제자의 의도가 아닌 것 같은데...
+# 이진수로 바꿔서 계산하기를 바랐던걸까?
